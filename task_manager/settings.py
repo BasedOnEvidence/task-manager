db5777d6dee7e3066d73798060db3d6c18fbe554
BasedOnEvidence <TestTypoTest@gmail.com>
2022-04-17 12:32:02 +0300

    Add localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'task-manager-template.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [

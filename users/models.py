eb2faa9a6392fdfbe789b71456973c4966d05b5b
user <user@TOPMO3UT.localdomain>
2022-08-22 12:33:06 +0300

    Return full name of user

--- users/models.py
+++ users/models.py
@@ -3,8 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    def full_name(self):
-        return self.get_full_name()
-
     def __str__(self):
-        return self.username
+        return self.get_full_name()

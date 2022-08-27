ac55bf90eb96795f83bbbd728992d48dfbc448b8
user <user@TOPMO3UT.localdomain>
2022-08-27 14:00:08 +0300

    Add filter checkbox

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,6 @@
+from django import forms
 from django.forms import ModelForm
-from django_filters import FilterSet
+from django_filters import FilterSet, BooleanFilter
 from django.utils.translation import gettext_lazy
 
 from tasks.models import Task
@@ -19,9 +20,23 @@ class TaskForm(ModelForm):
 
 
 class TasksFilter(FilterSet):
+
+    current_user_tasks = BooleanFilter(
+        label=gettext_lazy('Only my tasks'),
+        widget=forms.CheckboxInput(),
+        method='filter_current_user',
+        field_name='current_user_tasks'
+    )
+
+    def filter_current_user(self, queryset, field_name, box_is_checked):
+        current_user = self.request.user
+        if not current_user.is_anonymous and box_is_checked:
+            return queryset.filter(author=current_user)
+        return queryset
+
     class Meta:
         model = Task
-        fields = ['status', 'executor', 'labels']
+        fields = ['status', 'executor', 'labels', 'current_user_tasks']
         labels = {
             'status': gettext_lazy('Status'),
             'executor': gettext_lazy('Performer'),
